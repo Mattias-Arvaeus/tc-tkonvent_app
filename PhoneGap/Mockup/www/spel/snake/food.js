3d72941899dc4bea430093547cd5263f9dc35972
Anton Laurell <antonlaurell@hotmail.com>
2019-12-06 01:20:17 +0100

    snake now eats little rainbow parts

--- PhoneGap/Mockup/www/spel/snake/food.js
+++ PhoneGap/Mockup/www/spel/snake/food.js
@@ -1,13 +1,13 @@
 class Food {
-    constructor(rangex, rangey) {
+    constructor(rangex, rangey, color) {
         this.x = floor(random(rangex));
         this.y = floor(random(rangey));
         this.size = 1;
-        this.c = color(0, 0, 175);
+        this.c = color;
     }
 
     show() {
-        let edge_indent = 0.08;
+        let edge_indent = 0;
         fill(this.c);
         rect(this.x + edge_indent / 2, this.y + edge_indent / 2, this.size - edge_indent, this.size - edge_indent);
     }
