9f86675212ec8ce4e0c2f7f994802cc2ea51723f
Anton Laurell <antonlaurell@hotmail.com>
2019-12-07 09:58:17 +0100

    testing mobile swipe controls

--- kod/PhoneGap/www/spel/snake/sketch.js
+++ kod/PhoneGap/www/spel/snake/sketch.js
@@ -7,13 +7,13 @@ const pixel_size = vw / w;
 let setting_height = true;
 while (setting_height) {
     if (h * pixel_size > vh) {
-        console.log('reducing pixels from ' + h + ' to ' + (h - 1));
         h--;
     }
     else {
         setting_height = false;
     }
 }
+print('setting canvas height (pixels) to ' + h);
 let snake;
 let food;
 let boundaries = { xmin: 0, xmax: w, ymin: 0, ymax: h };
@@ -43,6 +43,7 @@ function new_food(body) {
 
 function setup() {
     createCanvas(w * pixel_size, h * pixel_size);
+    screen.orientation.lock('portrait');
     frameRate(fps);
 
     snake = new Snake(floor(w / 2), floor(h / 2), boundaries);
@@ -51,6 +52,49 @@ function setup() {
     window.start_text_element = document.getElementById("text-start");
     window.again_text_element = document.getElementById("text-again");
     window.start_text_element.style.display = 'block';
+}
+function detectswipe(el, func) {
+    swipe_det = new Object();
+    swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+    var min_x = 30;  //min x swipe for horizontal swipe
+    var max_x = 30;  //max x difference for vertical swipe
+    var min_y = 50;  //min y swipe for vertical swipe
+    var max_y = 60;  //max y difference for horizontal swipe
+    var direc = "";
+    ele = document.getElementById(el);
+    ele.addEventListener('touchstart', function (e) {
+        var t = e.touches[0];
+        swipe_det.sX = t.screenX;
+        swipe_det.sY = t.screenY;
+    }, false);
+    ele.addEventListener('touchmove', function (e) {
+        e.preventDefault();
+        var t = e.touches[0];
+        swipe_det.eX = t.screenX;
+        swipe_det.eY = t.screenY;
+    }, false);
+    ele.addEventListener('touchend', function (e) {
+        //horizontal detection
+        if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
+            if (swipe_det.eX > swipe_det.sX) direc = "r";
+            else direc = "l";
+        }
+        //vertical detection
+        else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
+            if (swipe_det.eY > swipe_det.sY) direc = "d";
+            else direc = "u";
+        }
+
+        if (direc != "") {
+            if (typeof func == 'function') func(el, direc);
+        }
+        direc = "";
+        swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+    }, false);
+}
+
+function myfunction(el, d) {
+    console.log("you swiped on element with id '" + el + "' to " + d + " direction");
 }
 
 function keyPressed() {
@@ -116,6 +160,8 @@ function draw() {
     noStroke();
     scale(pixel_size);
     background(0);
+
+    detectswipe('display', myfunction);
 
     if (snake.dir.x == 0 && snake.dir.y == 0 && snake.moved == true) {
         window.again_text_element.style.display = 'block';
