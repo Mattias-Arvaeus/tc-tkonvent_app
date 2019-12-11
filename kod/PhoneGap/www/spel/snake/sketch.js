8d7b01a32dae8f8145777a94143ec9b7a3afbf04
Anton Laurell <antonlaurell@hotmail.com>
2019-12-11 23:54:30 +0100

    score on background, tweaking menu

--- kod/PhoneGap/www/spel/snake/sketch.js
+++ kod/PhoneGap/www/spel/snake/sketch.js
@@ -83,7 +83,7 @@ function new_game(difficulty) {
     }
     game.started = true;
     frameRate(fps);
-    snake = new Snake(floor(w / 2), floor(h / 2), boundaries);
+    snake = new Snake(floor(w / 2), floor(h / 2) - 1, boundaries);
     new_food(snake.body);
 }
 
@@ -100,16 +100,18 @@ function setup() {
     button.template.x = vw * 0.5 - button.template.width * 0.5;
     button.template.strokeWeight = vw * 0.015;
     button.template.cornerRadius = 0;
+    button.template.color = "#fff";
+    button.template.textColor = "#333";
     button.template.textFont = "silkscreen";
     button.template.textSize = vw * 0.07;
     var btnspacing = vh * 0.03;
     button.template.onPress = function () {
         this.color = this.stroke;
-        this.textColor = "#ffffff";
+        this.textColor = "#fff";
     };
     button.template.onRelease = function () {
         if (!game.started) {
-            saved.difficulty = this["difficulty"];
+            saved.difficulty = this.difficulty;
             saved.stroke = this.stroke;
             game.guide = true;
             new_game(saved.difficulty);
@@ -126,28 +128,28 @@ function setup() {
     button.easy = { ...button.template };
     button.easy.y = centerbtns(4);
     button.easy.text = "easy";
-    button.easy.stroke = "#00ff00";
+    button.easy.stroke = "#0f0";
     button.easy.difficulty = difficulties.easy;
 
     // normal button
     button.normal = { ...button.template };
     button.normal.y = button.easy.y + button.template.height + btnspacing;
     button.normal.text = "normal";
-    button.normal.stroke = "#ffff00";
+    button.normal.stroke = "#ff0";
     button.normal.difficulty = difficulties.normal;
 
     // hard button
     button.hard = { ...button.template };
     button.hard.y = button.normal.y + button.template.height + btnspacing;
     button.hard.text = "hard";
-    button.hard.stroke = "#ff0000";
+    button.hard.stroke = "#f00";
     button.hard.difficulty = difficulties.hard;
 
     // insane button
     button.insane = { ...button.template };
     button.insane.y = button.hard.y + button.template.height + btnspacing;
     button.insane.text = "insane!!!";
-    button.insane.stroke = "#000000";
+    button.insane.stroke = "#333";
     button.insane.difficulty = difficulties.insane;
 
     // again button
@@ -166,7 +168,7 @@ function setup() {
     button.back = { ...button.template };
     button.back.y = button.again.y + button.template.height + btnspacing;
     button.back.text = "back";
-    button.back.stroke = "#ff66ff";
+    button.back.stroke = "#f6f";
     button.back.onRelease = function () {
         if (game.ended) {
             game.started = game.ended = false;
@@ -175,10 +177,10 @@ function setup() {
 
     // score button (not interactive)
     button.score = { ...button.template };
-    button.score.resize(button.template.width * 1.3, button.template.height * 1.6);
+    button.score.resize(button.template.width * 1.3, button.template.height * 1.3);
     button.score.y = button.again.y - button.score.height - btnspacing;
     button.score.x = vw * 0.5 - button.score.width * 0.5;
-    button.score.stroke = "#0000ff";
+    button.score.stroke = "#00f";
     button.score.onPress = function () { };
     button.score.onRelease = function () { };
 }
@@ -194,22 +196,23 @@ function draw() {
     if (!game.started) {
         drawbg();
         textSize(vw * 0.22);
-        text('snake', vw * 0.5, vh * 0.1)
+        text('snake', vw * 0.5, vh * 0.175)
         button.easy.draw();
         button.normal.draw();
         button.hard.draw();
         button.insane.draw();
     }
+
     // reset buttons manually since btn.onOutside does not work on mobile
     else {
-        button.easy.color = "#ffffff";
-        button.easy.textColor = "#000000";
-        button.normal.color = "#ffffff";
-        button.normal.textColor = "#000000";
-        button.hard.color = "#ffffff";
-        button.hard.textColor = "#000000";
-        button.insane.color = "#ffffff";
-        button.insane.textColor = "#000000";
+        button.easy.color = button.template.color;
+        button.easy.textColor = button.template.textColor;
+        button.normal.color = button.template.color;
+        button.normal.textColor = button.template.textColor;
+        button.hard.color = button.template.color;
+        button.hard.textColor = button.template.textColor;
+        button.insane.color = button.template.color;
+        button.insane.textColor = button.template.textColor;
     }
 
     // die screen
@@ -224,10 +227,10 @@ function draw() {
 
     // see line 165
     else {
-        button.again.color = "#ffffff";
-        button.again.textColor = "#000000";
-        button.back.color = "#ffffff";
-        button.back.textColor = "#000000";
+        button.again.color = button.template.color;
+        button.again.textColor = button.template.textColor;
+        button.back.color = button.template.color;
+        button.back.textColor = button.template.textColor;
     }
     scale(pixel_size); // resets after draw loop begins again
 
@@ -242,25 +245,34 @@ function draw() {
         if (game.guide && !game.ended) {
             textSize(w * 0.07);
             strokeWeight(0);
-            fill(0);
+            fill(80);
 
-            var txt = {
+            var guidetxt = {
                 x: w * 0.5,
-                y: h * 0.15
+                y: h * 0.3
             }
+
             if (snake.dir.x == 0 && snake.dir.y == 0) {
-                text('swipe to move!', txt.x, txt.y)
+                text('swipe to move!', guidetxt.x, guidetxt.y)
             }
             else if (snake.body.length == 1) {
-                text('eat to grow!', txt.x, txt.y)
+                text('eat to grow!', guidetxt.x, guidetxt.y)
             }
             else if (snake.body.length == 2) {
-                text('go as far\nas you can!', txt.x, txt.y)
+                text('go as far\nas you can!', guidetxt.x, guidetxt.y)
             }
         }
+
         if (snake.body.length >= 5) {
             game.guide = false;
         }
+
+        // display score
+        textSize(w * 0.4);
+        strokeWeight(0);
+        fill(80);
+        text(snake.body.length - 1, w * 0.5, h * 0.5);
+
 
         if (snake.did_eat(food)) {
             snake.body.push(snake.body[snake.body.length - 1]);
